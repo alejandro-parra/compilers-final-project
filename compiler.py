9cf75f009bc3669f1cc5b5cf1d1b8b10dd1e091f
Alejandro Parra Altamirano <alexparra_07@hotmail.com>
2021-06-11 23:05:55 -0500

    feature: yacc syntax analyzer

--- compiler.py
+++ compiler.py
@@ -90,3 +90,186 @@ def t_error(t):
 
 # Inicializamos el analizador léxico
 lexer = lex.lex()
+
+# ----------------------------------ANALISIS SINTÁCTICO---------------------------------------
+
+# Reglas de precedencia. Igual, se manejan tuplas porque es contenido que no se modificará.
+precedence = (
+    ('left', 'AND', 'OR'),
+    ('nonassoc', 'EQUALS', 'NOT_EQUALS', 'EQ_MORE', 'EQ_LESS', 'MORE', 'LESS'),
+    ('left', 'PLUS', 'MINUS'),
+    ('left', 'MULT', 'DIV'),
+    ('left', 'EXP'),
+    ('right', 'UMINUS'),  # Uminus es el operador para decir que un número es negativo
+)
+
+# Tablas de variables con sus valores, sus tipos y registro de las instrucciones
+instructions = ()
+
+# Listado de las reglas de producción de mi lenguaje
+
+# Producción inicial
+
+
+def p_start(p):
+    '''start : statement'''
+    global instructions
+    instructions = p[1]
+
+
+def p_statement(p):
+    '''statement : print_stmt FINISH statement
+                | register_stmt FINISH statement
+                | condition_stmt statement
+                | for_stmt statement
+                | while_stmt statement
+                | empty'''
+    if len(p) > 2:
+        if p[2] == ';':
+            p[2] = p[3]
+        p[0] = (p[1],) + p[2]
+    else:
+        p[0] = ()
+
+
+def p_expression_operation(p):
+    '''expression : expression AND expression
+                | expression OR expression
+                | expression PLUS expression
+                | expression MINUS expression
+                | expression MULT expression
+                | expression DIV expression
+                | expression EXP expression
+                | expression EQUALS expression
+                | expression NOT_EQUALS expression
+                | expression EQ_MORE expression
+                | expression EQ_LESS expression
+                | expression MORE expression
+                | expression LESS expression'''
+    p[0] = ('operation', p[1], p[2], p[3])
+
+
+def p_expression_id(p):
+    "expression : ID"
+    p[0] = p[1]
+
+
+def p_expression_val(p):
+    '''expression : FLOAT_VAL
+                 | INT_VAL
+                 | STR_VAL
+                 | bool_val'''
+    p[0] = p[1]
+
+
+def p_bool_val(p):
+    '''bool_val : TRUE
+              | FALSE'''
+    if p[1] == "true":
+        p[0] = True
+    elif p[1] == "false":
+        p[0] = False
+
+
+def p_expression_parenthesis(p):
+    'expression : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+
+def p_expression_uminus(p):
+    'expression : MINUS expression %prec UMINUS'
+    p[0] = -p[2]
+
+
+def p_print_stmt(p):
+    'print_stmt : PRINT expression'
+    p[0] = ('print', p[2])
+
+
+def p_register_stmt(p):
+    '''register_stmt : declare_reg
+            | declare_assign_reg
+            | assign_reg'''
+    p[0] = p[1]
+
+
+def p_condition_stmt(p):
+    '''condition_stmt : if_cond elif_cond else_cond'''
+    p[0] = ('condition', p[1], p[2], p[3])
+
+
+def p_for_stmt(p):
+    '''for_stmt : FOR LPAREN declare_assign_reg FINISH expression FINISH assign_reg RPAREN LKEY statement RKEY'''
+    p[0] = ('for', p[3], p[5], p[7], p[10])
+
+
+def p_while_stmt(p):
+    '''while_stmt : WHILE LPAREN expression RPAREN LKEY statement RKEY
+                 | DO LKEY statement RKEY WHILE LPAREN expression RPAREN FINISH'''
+    if p[1] == "while":
+        p[0] = ('while', p[3], p[6])
+    else:
+        p[0] = ('do-while', p[7], p[3])
+
+
+def p_empty(p):
+    'empty :'
+    pass
+
+
+def p_type(p):
+    '''type : BOOL
+           | INT
+           | FLOAT
+           | STRING'''
+    p[0] = p[1]
+
+
+def p_declare_reg(p):
+    '''declare_reg : type ID'''
+    p[0] = ('declare', p[1], p[2])
+
+
+def p_declare_assign_reg(p):
+    '''declare_assign_reg : type ID ASSIGN expression'''
+    p[0] = ('declare_assign', p[1], p[2], p[4])
+
+
+def p_assign_reg(p):
+    '''assign_reg : ID ASSIGN expression'''
+    p[0] = ('assign', p[1], p[3])
+
+
+def p_if_cond(p):
+    '''if_cond : IF LPAREN expression RPAREN LKEY statement RKEY'''
+    p[0] = ('if', p[3], p[6])
+
+
+def p_elif_cond(p):
+    '''elif_cond : ELIF LPAREN expression RPAREN LKEY statement RKEY elif_cond
+                | empty'''
+    if len(p) > 2:
+        p[0] = (('elif', p[3], p[6]), ) + p[8]
+    else:
+        p[0] = ()
+
+
+def p_else_cond(p):
+    '''else_cond : ELSE LKEY statement RKEY
+                | empty'''
+    if len(p) > 2:
+        p[0] = ('else', p[3])
+
+
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+
+# Iniciamos el analizador sintactico y procesamos el archivo input para generar el arbol
+yacc.yacc()
+file = open("input.txt", "r")
+s = file.read()
+yacc.parse(s)
